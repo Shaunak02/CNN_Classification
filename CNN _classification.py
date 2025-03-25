0ff6a6ce7d515896b07e3b9c6d4bfea9a869c5df
Shaunak02 <shaunak.bhalerao10@gmail.com>
2025-03-25 12:59:40 +0100

    Initial commit

---
+++ CNN _classification.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D(2,2),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')  # 10 classes for CIFAR-10
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#model.summary()
+
+history = model.fit(x_train, y_train, epochs=10)
+
+model.save("cnn_model.h5")
+print("Model saved successfully!")
---
+++ evaluate model.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+print(os.environ["PATH"])
+
+# import numpy as np
+# from tensorflow.keras.models import load_model
+# model = load_model("cnn_model.h5")
+
+
+# # Load the new image
+# img = cv2.imread("IMG_20201223_093518.jpg")  # Replace with your image path
+# img = cv2.resize(img, (32, 32))    # Resize to match the input size of the model
+# img = img.astype("float32") / 255  # Normalize pixel values (0-1)
+# img = np.expand_dims(img, axis=0)  # Add batch dimension
+
+# # Predict the class
+# predictions = model.predict(img)
+# predicted_class = np.argmax(predictions)
+# print(f"Predicted Class: {predicted_class}")
